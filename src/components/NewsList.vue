b0722d3f2b09544ee77f75be49f30c82734fe3f5
LEGEN DARY MAD <tajay59@gmail.com>
2022-04-04 13:27:01 -0400

    Vue app created

---
+++ src/components/NewsList.vue
@@ -0,0 +1,22 @@
+<template>
+      <ul>
+          <li class="news__list">News item 1</li>
+          <li class="news__list">News item 2</li>
+          <li class="news__list">News item 3</li>
+      </ul>
+</template>
+
+<script>
+export default {
+    data() {
+      return {};       
+    },
+};
+</script>
+
+<style>
+/* Add any component specific styles here */
+li{
+    list-style: none; 
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script>
+import NewsList from '@/components/NewsList.vue';
 export default {
     data() {
         return {
-            message: "Hello World! Welcome to VueJS"
-        };
-    },
+            message: "Hello World I am a Matian. Welcome to VueJS"
+               };
+           },
+    components: {NewsList}
 }
 </script>
 
@@ -13,10 +15,13 @@ export default {
       <div class="text-center">
         <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
         <h1>{{ message }}</h1>
+        <NewsList/>
       </div>
     </div>
 </template>
 
 <style>
 /* Add any component specific styles here */
+
+
 </style>
