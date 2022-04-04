bdcd76eebf87e74c7c4957b3384e5986d7f6d785
LEGEN DARY MAD <tajay59@gmail.com>
2022-04-04 16:59:42 -0400

    Part 2 complete

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,4 +1,13 @@
 <template>
+    <form @submit.prevent="searchNews" class="d-flex flex-column justify-content-center">
+        <div class="input-group mx-sm-3 mb-2">
+            <label class="visually-hidden" for="search">Search</label>
+            <input type="search" name="search" v-model="searchTerm" id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
+            <button class="btn btn-primary mb-2">Search</button>
+        </div>
+        <p>You are searching for {{ searchTerm }}</p>
+    </form>
+
       <ul class="news__list">
           <li v-for="article in articles" class="news__item">
               <div class="card">
@@ -8,18 +17,20 @@
                     <div class="content"> {{article.content}}</div>
                   </div>
                 
-              </div>
-              
-              
+              </div>   
               </li>
       </ul>
+
 </template>
 
 <script>
 console.log( import.meta.env)
 export default {
     
-    data() {  return { articles:[]}; },
+    data() {  return { 
+        articles:[],
+        searchTerm:""
+        }; },
 
     created() { 
         let self = this;
@@ -32,7 +43,23 @@ export default {
             console.log(data);
             self.articles = data.articles;
             });
+            },
+
+    methods: {
+        searchNews() {
+            let self = this;
+            fetch(`https://newsapi.org/v2/everything?q=${self.searchTerm}&language=en`, {
+            headers: { 'Authorization': `Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`}})
+            .then(function(response) {  return response.json(); })
+            .then(function(data) {
+                console.log(data);
+                self.articles = data.articles;
+                });
             }
+        }
+
+
+
 };
 </script>
 
@@ -88,7 +115,7 @@ li{
     font-size: 20px;
 }
 .content{
-    color: blueviolet;
+    color: gray;
     word-wrap: break-word;
     text-align: justify;
 }
